8da1ec5a30840a8be5a523ff9bcd317a0fa31033
aaamur <ponch310593>
2017-03-19 19:07:53 +0700

    first commit

---
+++ src/main/java/QuizCard.java
@@ -0,0 +1,21 @@
+/**
+ * Created by sasha on 19.03.17.
+ */
+public class QuizCard {
+    private String question;
+    private String answer;
+
+    public QuizCard(String question, String answer) {
+        this.question = question;
+        this.answer = answer;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+}
